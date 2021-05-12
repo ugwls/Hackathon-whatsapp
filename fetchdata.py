565e61c6b8e177648796162792458a6b63d05d70
Ujjwal Sharma <46123077+ujjwalvats16@users.noreply.github.com>
2021-05-12 19:35:49 +0530

    Update fetchdata.py

--- fetchdata.py
+++ fetchdata.py
@@ -1,5 +1,5 @@
-import json
 import requests
+import json
 headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 url = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=110001&date=31-03-2021"

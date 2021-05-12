079bf793f557df2746023a06a195ee8410dc359c
Ujjwal Sharma <46123077+ujjwalvats16@users.noreply.github.com>
2021-05-12 17:01:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -83,4 +83,4 @@ enter your pincode and date separated by comma, for example if your pincode is 1
 
 #headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 if __name__ == "__main__":
-    app.run(port=5000)
+    app.run()

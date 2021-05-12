043c0067abcb376b09c5d9af07bfa1c4f54060fe
Ujjwal Sharma <46123077+ujjwalvats16@users.noreply.github.com>
2021-05-12 23:41:19 +0530

    Update fetchdata.py

--- fetchdata.py
+++ fetchdata.py
@@ -1,9 +1,9 @@
 import requests
 import json
 
-
+header={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 def get_data(pin, date):
-    r = requests.get("https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode="+pin+"&date="+date, headers={'User-Agent': 'Mozilla/5.0'})
+    r = requests.get("https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode="+pin+"&date="+date, headers=header)
     check = r.text
     print(check)
     data = json.loads(check)

ea1689f2d3af81a38d50d0863254358f6b9a8803
Ujjwal Sharma <46123077+ujjwalvats16@users.noreply.github.com>
2021-05-12 20:48:38 +0530

    Update fetchdata.py

--- fetchdata.py
+++ fetchdata.py
@@ -1,23 +1,21 @@
-from urllib.request import Request, urlopen
-import json
+import requests
 
 
 def get_data(pin, date):
-    print(pin,date)
-    try:
-        req = Request('https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode='+pin+"&date="+date, headers={'User-Agent': 'Mozilla/5.0'})
-        webpage = urlopen(req).read()
-        print(webpage)
-        data = json.loads(webpage)
+    r = requests.get("https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode="+pin+"&date="+date, headers={'User-Agent': 'Mozilla/5.0'})
+    data = r.json()
+    check = r.text
+    if 'Invalid Pincode' in check:
+        return "invalid pincode"
+    else:
         centers = data["centers"]
         if (len(centers) > 0):
             data_all = []
             for centre in centers:
                 sessions = centre["sessions"]
                 for session in sessions:
-                    data_all.append({"centre_name": centre["name"], "centre_address": centre["address"],"availability": session["available_capacity"], "date": session["date"]})
+                    data_all.append({"centre_name": centre["name"], "centre_address": centre["address"],
+                                     "availability": session["available_capacity"], "date": session["date"]})
             return (data_all)
         else:
             return ("no centre")
-    except:
-        return "invalid pincode"
